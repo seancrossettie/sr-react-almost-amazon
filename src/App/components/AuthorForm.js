2542182c47aa0737a6d2b250845a2823d0f5a93a
Sean <jb@JBs-MacBook-Pro.local>
2021-04-24 11:47:51 -0500

    setup docs

---
+++ src/App/components/AuthorForm.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import addAuthor from '../data/authorData';
+
+export default function AuthorForm() {
+  const [author, setAuthor] = useState({
+    name: '',
+    email: '',
+  });
+
+  const handleInputChange = (e) => {
+    setAuthor((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addAuthor(author);
+  };
+
+  return (
+    <>
+      <div className='add-author-form'>
+        <form
+          id='add-author-form'
+          autoComplete='off'
+          onSubmit={handleSubmit}
+        >
+        <h2>Add an Author</h2>
+        <label>Name: </label>
+        <input
+          name='name'
+          type='text'
+          placeholder='Name'
+          value={author.name}
+          onChange={handleInputChange}
+          ></input>
+          <label>Email: </label>
+        <input
+          name='email'
+          type='email'
+          placeholder='Email'
+          value={author.email}
+          onChange={handleInputChange}
+          ></input>
+          <button type='submit'>Submit</button>
+        </form>
+      </div>
+    </>
+  );
+}
--- src/App/index.js
+++ src/App/index.js
@@ -1,36 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.scss';
+import firebase from 'firebase';
+import firebaseConfig from './data/apiKeys';
+import AuthorForm from './components/AuthorForm';
 
 function App() {
-  const [domWriting, setDomWriting] = useState('Nothing Here!');
-
-  const handleClick = (e) => {
-    console.warn(`You clicked ${e.target.id}`);
-    setDomWriting(`You clicked ${e.target.id}! Check the Console!`);
-  };
+  firebase.initializeApp(firebaseConfig);
 
   return (
     <div className='App'>
-      <h2>INSIDE APP COMPONENT</h2>
-      <div>
-        <button
-          id='this-button'
-          className='btn btn-info'
-          onClick={handleClick}
-        >
-          I am THIS button
-        </button>
-      </div>
-      <div>
-        <button
-          id='that-button'
-          className='btn btn-primary mt-3'
-          onClick={handleClick}
-        >
-          I am THAT button
-        </button>
-      </div>
-      <h3>{domWriting}</h3>
+      <AuthorForm />
     </div>
   );
 }
