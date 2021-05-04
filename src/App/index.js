5e07affccd0b45b44540b8e55fae4cf4cd52cd3b
Sean <jb@JBs-MacBook-Pro.local>
2021-05-03 19:51:39 -0500

    added auth

--- src/App/index.js
+++ src/App/index.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import { BrowserRouter as Router } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 import Routes from '../helpers/Router';
@@ -6,15 +8,32 @@ import { getAuthors } from '../helpers/data/authorData';
 
 function App() {
   const [authors, setAuthors] = useState([]);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     getAuthors().then(setAuthors);
   }, []);
 
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((authed) => {
+      if (authed) {
+        const userObj = {
+          fullName: authed.displayName,
+          profileImage: authed.photoURL,
+          uid: authed.uid,
+          user: authed.email.split('@')[0]
+        };
+        setUser(userObj);
+      } else if (user || user === null) {
+        setUser(false);
+      }
+    });
+  }, []);
+
   return (
     <>
       <Router>
-        <NavBar />
+        <NavBar user={user} />
         <Routes authors={authors} setAuthors={setAuthors} />
       </Router>
     </>
